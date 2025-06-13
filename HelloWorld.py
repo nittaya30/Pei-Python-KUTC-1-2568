b9d54288323f5c1ed737b741e3a1b6c7a809d403
nittaya30 <nittayasaladaeng31@gmail.com>
2025-06-13 14:36:03 +0700

    first commit

---
+++ HelloWorld.py
@@ -0,0 +1,30 @@
+print("Hello")
+#คำสั่งในการแสดงผลข้อความ
+
+number = 10 # ตัวแปรประเภทจำนวนเต็ม Integer
+
+str(number) # เปลี่ยนจาก int เป็น String
+# การ Casing แปลงชนิดของข้อมูล
+
+
+total = 97.44 # ตัวแปรประเภททศนิยม Float
+float(total)
+
+name ="Nittaya" # ตัวแปรประเภทข้อความ String
+status = True
+# ตัวแปรประเภทตรระศาสตร์
+# Ture = จริง
+# False = เท็จ
+
+print(number,total,name,status)
+
+# การดำเนินทางคณิตศาสตร์
+score = 2+2/3
+print(score)
+
+nickname = 1
+str(nickname )
+
+
+print(nickname + nickname)
+print(str(nickname )+ str(nickname))
